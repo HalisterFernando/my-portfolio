47892c1d41a749cf2376826bb29297b62fb39c32
Seu nome <halisterfernando@hotmail.com>
2022-08-17 04:57:37 -0300

    feat: :sparkles: trybe wallet adicionado ao portfolio

--- src/cardImages.js
+++ src/cardImages.js
@@ -1,11 +1,13 @@
 import Classi from './images/ClassiCar.png';
 import Trivia from './images/NeonTrivia.png';
 import Tunes from './images/TrybeTunes.png';
+import Wallet from './images/TrybeWallet.png';
 
 const images = [
   { name: 'Trybe Tunes', link: 'https://trybe-tunes-ten.vercel.app/', image: Tunes },
   { name: 'ClassiCar', link: 'https://classi-car.herokuapp.com/', image: Classi },
   { name: 'Neon Trivia', link: 'https://neon-trivia-xi.vercel.app/', image: Trivia },
+  { name: 'Trybe Wallet', link: 'https://trybe-wallet-puce.vercel.app/', image: Wallet },
 ];
 
 export default images;

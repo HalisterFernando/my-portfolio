1df0b768767611fda264ba292b231ce40b819cad
Halister Fernando <halisterfernando@hotmail.com>
2023-05-08 11:29:41 -0300

    chore: alterado link para classicar

--- src/cardImages.js
+++ src/cardImages.js
@@ -5,7 +5,7 @@ import Wallet from './images/TrybeWallet.png';
 
 const images = [
   { name: 'Trybe Tunes', link: 'https://trybe-tunes-ten.vercel.app/', image: Tunes },
-  { name: 'ClassiCar', link: 'https://classi-car.herokuapp.com/', image: Classi },
+  { name: 'ClassiCar', link: 'https://classi-car.vercel.app/', image: Classi },
   { name: 'Neon Trivia', link: 'https://neon-trivia-xi.vercel.app/', image: Trivia },
   { name: 'Trybe Wallet', link: 'https://trybe-wallet-puce.vercel.app/', image: Wallet },
 ];
